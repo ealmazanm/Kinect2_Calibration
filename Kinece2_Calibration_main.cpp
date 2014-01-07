60798b950de2abb045e22add16c276f1e54da28a
ealmazanm <emilio.almazan@kingston.ac.uk>
2014-01-07 15:51:04 +0000

    Free transformation of points

--- Kinece2_Calibration_main.cpp
+++ Kinece2_Calibration_main.cpp
@@ -49,6 +49,7 @@ void updateActivityMap(const XnPoint3D* p3D, const int nP, int idKinect)
 XnPoint3D p3D[3];
 KinectSensor kinects[NUM_SENSORS];
 Mat depthImages[NUM_SENSORS];
+Mat rgbImages[NUM_SENSORS];
 const XnDepthPixel* depthMaps[NUM_SENSORS];
 char* nWindows[NUM_SENSORS];
 
@@ -87,6 +88,7 @@ void pointSelection_onMouse_single(int event, int x, int y, int flags, void* par
 	}
 }
 
+int nPointsTotal = 0;
 void pointSelection_onMouse(int event, int x, int y, int flags, void* param)
 {
 	
@@ -111,8 +113,9 @@ void pointSelection_onMouse(int event, int x, int y, int flags, void* param)
 					outFPK2 << (float)p3D[2].X << " " << (float)p3D[2].Y << " " << (float)p3D[2].Z << endl;
 
 				p3D[*id].X = -1;
-				circle(depthImages[*id], Point(x,y), 2, Scalar(255,0,0));
-				imshow(nWindows[*id], depthImages[*id]);
+				circle(rgbImages[*id], Point(x,y), 2, Scalar(0,0,255));
+				imshow(nWindows[*id], rgbImages[*id]);
+				cout << "Point selected "  << ++nPointsTotal << endl;
 				waitKey(0);
 			}
 			
@@ -137,9 +140,136 @@ void createDepthMatrix(const XnDepthPixel* dMap, Mat& depthMat)
 	}
 
 }
+
+void combineRGBD(Mat& img, const XnDepthPixel* dMap)
+{
+	uchar* data = img.data; 
+	int step = img.step;
+	for (int y = 0; y < XN_VGA_Y_RES; y++)
+	{
+		uchar* ptr = data + y*step;
+		for (int x = 0; x < XN_VGA_X_RES; x++)
+		{
+			int depth = dMap[y*XN_VGA_X_RES+x];
+			if (depth == 0)
+			{
+				ptr[3*x] = 255; ptr[3*x + 1] = 255; ptr[3*x + 2] = 255;
+			}
+		}
+	}
+}
+
 ifstream tiltTXT("D:\\CameraCalibrations\\extrinsics\\tilt.txt");
 
-int main(int argc, char* argv[])
+void pointCorrespondences()
+{
+	int tilt = 0;
+	tiltTXT >> tilt;
+	char* paths[3];
+/*	paths[0] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect0_calib.oni";
+	paths[1] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect1_calib.oni";
+	paths[2] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect2_calib.oni";
+*/
+	paths[0] = "d:/Emilio/Tracking/DataSet/kinect0_calib.oni";
+	paths[1] = "d:/Emilio/Tracking/DataSet/kinect1_calib.oni";
+	paths[2] = "d:/Emilio/Tracking/DataSet/kinect2_calib.oni";
+
+//	p3D[0].X = -1; p3D[1].X = -1;
+	
+	const XnRGB24Pixel* rgbMaps[NUM_SENSORS];
+
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		kinects[i].initDevice(i, REF_CAM, true, paths[i]);
+		kinects[i].startDevice();
+		kinects[i].tilt(tilt);
+	}
+
+	nWindows[0] =  "depth 0";
+	nWindows[1] =  "depth 1";
+	nWindows[2] =  "depth 2";
+
+	namedWindow(nWindows[0]);
+	namedWindow(nWindows[1]);
+	namedWindow(nWindows[2]);
+//	Point p0, p1;
+//	p1.x = p0.x = -1; p1.y =  p0.y = -1;
+	int id0 = 0;
+	int id1 = 1;
+	int id2 = 2;
+	//Select points for calibrations
+	cvSetMouseCallback(nWindows[0], pointSelection_onMouse, (int*)&id0);
+	cvSetMouseCallback(nWindows[1], pointSelection_onMouse, (int*)&id1);
+	cvSetMouseCallback(nWindows[2], pointSelection_onMouse, (int*)&id2);
+	
+
+	//flags
+	bool bShouldStop = false;
+
+	
+	Mat depthMat[NUM_SENSORS];
+
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		depthImages[i] = Mat(XN_VGA_Y_RES, XN_VGA_X_RES, CV_8UC3);
+		rgbImages[i] = Mat(XN_VGA_Y_RES, XN_VGA_X_RES, CV_8UC3);
+		depthMat[i] = Mat(XN_VGA_Y_RES, XN_VGA_X_RES, CV_16U);
+	}
+
+	KINECTS_DISPLACEMENT = max(abs(kinects[0].translation(0)), abs(kinects[2].translation(0))); //MAXIMUM TRANSLATION IN THE HORIZONTAL AXIS
+	MAX_RANGE = ActivityMap_Utils::MAX_Z_TRANS + KINECTS_DISPLACEMENT; 
+	bool first = true;
+
+	int waitTime = 1;
+
+	int nPoints = 0;
+	while (!bShouldStop)
+	{		
+		cout << "Frames: " << frames << endl;
+
+		for (int i = 0; i < NUM_SENSORS; i++)
+			kinects[i].waitAndUpdate();
+		
+		for (int i = 0; i < NUM_SENSORS;  i++)
+		{
+			depthMaps[i] = kinects[i].getDepthMap();
+			rgbMaps[i] = kinects[i].getRGBMap();
+			//new part
+			kinects[i].getDepthImage(depthImages[i]);
+			kinects[i].getRGBImage(rgbImages[i]);
+		}
+
+		
+		
+		//for (int i = 0; i < NUM_SENSORS; i++)
+		//{
+		//	combineRGBD(rgbImages[i], depthMaps[i]);
+		//}
+
+		imshow(nWindows[0], rgbImages[0]);
+		imshow(nWindows[1], rgbImages[1]);
+		imshow(nWindows[2], rgbImages[2]);
+		int c = waitKey(waitTime);
+		if (c == 13)
+			waitTime = 0;
+		
+		if (c == 27)
+			bShouldStop = true;
+		
+		cout << "Frame: " << frames << endl;
+		frames++;
+	}
+
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		kinects[i].stopDevice();
+  		kinects[i].shutDown();
+	}
+}
+
+
+//Print the foreground point cloud
+void pointCloudCorrespondence()
 {
 	int tilt = 0;
 	tiltTXT >> tilt;
@@ -172,35 +302,12 @@ int main(int argc, char* argv[])
 	namedWindow(nWindows[0]);
 	namedWindow(nWindows[1]);
 	namedWindow(nWindows[2]);
-	Point p0, p1;
-	p1.x = p0.x = -1; p1.y =  p0.y = -1;
-	int id0 = 0;
-	int id1 = 1;
-	int id2 = 2;
-	//Select points for calibrations
-	//cvSetMouseCallback(nWindows[0], pointSelection_onMouse, (int*)&id0);
-	//cvSetMouseCallback(nWindows[1], pointSelection_onMouse, (int*)&id1);
-	//cvSetMouseCallback(nWindows[2], pointSelection_onMouse, (int*)&id2);
 	
-
-	//Check calibration accuracy
-	int ids01[2] = {id0, id1};
-	int ids10[2] = {id1, id0};
-	int ids12[2] = {id1, id2};
-	int ids21[2] = {id2, id1};
-	//01 and 10
-	cvSetMouseCallback(nWindows[0], pointSelection_onMouse_single, ids01);
-	cvSetMouseCallback(nWindows[1], pointSelection_onMouse_single, ids10);
-	//12 and 21
-	//cvSetMouseCallback(nWindows[2], pointSelection_onMouse_single, ids21);
-	//cvSetMouseCallback(nWindows[1], pointSelection_onMouse_single, ids12);
-	
-
 	//flags
 	bool bShouldStop = false;
 	bool bgComplete = true;
 
-	Mat rgbImages[NUM_SENSORS];
+	//Mat rgbImages[NUM_SENSORS];
 	Mat depthMat[NUM_SENSORS];
 	Mat masks[NUM_SENSORS];
 	Mat grey;
@@ -249,8 +356,6 @@ int main(int argc, char* argv[])
 			cvtColor(depthImages[i],grey,CV_RGB2GRAY);
 			masks[i] = grey > 250; //mask that identifies the noise (1)
 		}
-		
-
 		nPoints = 0;
 		if (bgComplete)// && frames > 84) //Trans must be true
 		{
@@ -264,10 +369,9 @@ int main(int argc, char* argv[])
 					points3D[i] = new XnPoint3D[totalPoints];
 					kinects[i].arrayBackProject(pointsFore2D[i], points3D[i], totalPoints);
 					
-					kinects[i].tiltCorrection(points3D[i], totalPoints);
-
-
-	/*				if (frames == debugFrame)
+					//kinects[i].tiltCorrection(points3D[i], totalPoints);
+					//frame 809 to see how the x spreads with distance
+					if (frames == 729)
 					{
 						ofstream* out;
 						if (i == 0)
@@ -285,53 +389,21 @@ int main(int argc, char* argv[])
 								*out << (float)p.X << " " << (float)p.Y << " " << (float)p.Z << endl;
 							}
 						}
-					}*/
+					}
 					delete []points3D[i];
 					
 				}
 			}
 		}
 
+		for (int i = 0; i < NUM_SENSORS; i++)
+		{
+			combineRGBD(rgbImages[i], depthMaps[i]);
+		}
 
-		//for (int i = 0; i < 480; i++)
-		//{
-		//	int depth = depthMaps[0][i*XN_VGA_X_RES+615];
-		//	if (depth != 0)
-		//	{
-		//		XnPoint3D edge2D[1];
-		//		XnPoint3D edge3D[1];
-		//		edge2D[0].X = 615; edge2D[0].Y = i; edge2D[0].Z = depth;
-		//		kinects[0].arrayBackProject(edge2D, edge3D, 1);
-		//		kinects[0].transformArrayNoTilt(edge3D, 1);
-		//		XnPoint3D* edge2Dp = kinects[1].arrayProject(edge3D, 1);
-		//		Point p = Point(edge2Dp->X, edge2Dp->Y);
-		//		circle(depthImages[1], p, 2, Scalar(255,0,0));
-		//		//XnPoint3D edge3D[1];
-		//		//edge3D[0].X = depth*tanf(28.5*CV_PI/180);
-		//		//float range = sqrtf( powf(depth,2) + powf(edge3D[0].X,2) );
-		//		////edge3D[0].Y = range * tanf(19.09*CV_PI/180);
-		//		//edge3D[0].Y = depth * tanf(21.5*CV_PI/180);
-		//		//edge3D[0].Z = depth;
-	
-		//		//kinects[0].transformArrayNoTilt(edge3D, 1);
-		//		//XnPoint3D* edge2D = kinects[1].arrayProject(edge3D, 1);
-		//		//Point p = Point(edge2D->X, edge2D->Y);
-		//		//circle(depthImages[1], p, 2, Scalar(255,0,0));
-
-		//	}
-		//}
-
-		//line(depthImages[0], Point(615, 0), Point(615, 480), Scalar(0,0,255));
-	
-	//	line(depthImages[1], p1,p2, Scalar(0,0,255));
-	//	line(depthImages[1], p3,p4, Scalar(0,0,255));
-
-		line(depthImages[1], Point(121, 0), Point(241,480), Scalar::all(0));
-		line(depthImages[1], Point(599, 0), Point(499,480), Scalar::all(0));
-
-		imshow(nWindows[0], depthImages[0]);
-		imshow(nWindows[1], depthImages[1]);
-		imshow(nWindows[2], depthImages[2]);
+		imshow(nWindows[0], rgbImages[0]);
+		imshow(nWindows[1], rgbImages[1]);
+		imshow(nWindows[2], rgbImages[2]);
 		int c = waitKey(waitTime);
 		if (c == 13)
 		{
@@ -349,6 +421,110 @@ int main(int argc, char* argv[])
 		kinects[i].stopDevice();
   		kinects[i].shutDown();
 	}
+}
+
+void checkCalibration()
+{
+	int tilt = 0;
+	tiltTXT >> tilt;
+	char* paths[3];
+/*	paths[0] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect0_calib.oni";
+	paths[1] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect1_calib.oni";
+	paths[2] = "d:/Emilio/Tracking/DataSet/sb125/CalibrationVideos/kinect2_calib.oni";
+*/
+	paths[0] = "d:/Emilio/Tracking/DataSet/kinect0_calib.oni";
+	paths[1] = "d:/Emilio/Tracking/DataSet/kinect1_calib.oni";
+	paths[2] = "d:/Emilio/Tracking/DataSet/kinect2_calib.oni";
+	
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		kinects[i].initDevice(i, REF_CAM, true, paths[i]);
+		kinects[i].startDevice();
+		kinects[i].tilt(tilt);
+	}
+
+	nWindows[0] =  "depth 0";
+	nWindows[1] =  "depth 1";
+	nWindows[2] =  "depth 2";
+
+	namedWindow(nWindows[0]);
+	namedWindow(nWindows[1]);
+	namedWindow(nWindows[2]);
+
+	int id0 = 0;
+	int id1 = 1;
+	int id2 = 2;
+	//Check calibration accuracy
+	int ids01[2] = {id0, id1};
+	int ids10[2] = {id1, id0};
+	//int ids12[2] = {id1, id2};
+	//int ids21[2] = {id2, id1};
+	//01 and 10
+	cvSetMouseCallback(nWindows[0], pointSelection_onMouse_single, ids01);
+	cvSetMouseCallback(nWindows[1], pointSelection_onMouse_single, ids10);
+	//12 and 21
+	//cvSetMouseCallback(nWindows[2], pointSelection_onMouse_single, ids21);
+	//cvSetMouseCallback(nWindows[1], pointSelection_onMouse_single, ids12);
+	
+	//flags
+	bool bShouldStop = false;
+	//Mat rgbImages[NUM_SENSORS];
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		depthImages[i] = Mat(XN_VGA_Y_RES, XN_VGA_X_RES, CV_8UC3);
+		rgbImages[i] = Mat(XN_VGA_Y_RES, XN_VGA_X_RES, CV_8UC3);
+	}
+
+	KINECTS_DISPLACEMENT = max(abs(kinects[0].translation(0)), abs(kinects[2].translation(0))); //MAXIMUM TRANSLATION IN THE HORIZONTAL AXIS
+	MAX_RANGE = ActivityMap_Utils::MAX_Z_TRANS + KINECTS_DISPLACEMENT; 
+	bool first = true;
+
+	int waitTime = 1;
+	while (!bShouldStop)
+	{		
+		cout << "Frames: " << frames << endl;
+
+		for (int i = 0; i < NUM_SENSORS; i++)
+			kinects[i].waitAndUpdate();
+		
+		for (int i = 0; i < NUM_SENSORS;  i++)
+		{
+			depthMaps[i] = kinects[i].getDepthMap();
+			//new part
+			kinects[i].getDepthImage(depthImages[i]);
+			kinects[i].getRGBImage(rgbImages[i]);
+			combineRGBD(rgbImages[i], depthMaps[i]);
+		}
+		
+		imshow(nWindows[0], depthImages[0]);
+		imshow(nWindows[1], depthImages[1]);
+		imshow(nWindows[2], depthImages[2]);
+		int c = waitKey(waitTime);
+		if (c == 13)
+			waitTime = 0;
+		
+		if (c == 27)
+			bShouldStop = true;
+		
+		cout << "Frame: " << frames << endl;
+		frames++;
+	}
+
+	for (int i = 0; i < NUM_SENSORS; i++)
+	{
+		kinects[i].stopDevice();
+  		kinects[i].shutDown();
+	}
+}
+int main(int argc, char* argv[])
+{
+
+	pointCorrespondences();
+
+	//pointCloudCorrespondence();
+
+	//checkCalibration();
+
 	return 0;
 
 }
